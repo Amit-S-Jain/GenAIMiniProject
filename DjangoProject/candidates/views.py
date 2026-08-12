143c6f77b8293d77015032210dc2e27e2bbdebe1
Amit <Amitsjain9161@gmail.com>
2026-08-12 23:47:05 +0530

    Removed unnecessary comments and starting new project following Agent architecture

--- DjangoProject/candidates/views.py
+++ DjangoProject/candidates/views.py
@@ -4,8 +4,6 @@ from django.urls import reverse
 from django.http import HttpResponse
 from .forms import ResumeUploadForm, CandidateForm
 from AIEngine.app import MainClass   # <-- your custom parsing function
-import json
-import tempfile
 from django.conf import settings
 from pathlib import Path
 
@@ -44,54 +42,10 @@ def upload_resume(request):
         form = ResumeUploadForm()
     return render(request, "candidates/upload_resume.html", {"form": form})
 
-    #         return render(
-    #             request,
-    #             "candidates/success.html",
-    #             {"form": form, "parsed": parsed_data, "saved_path": file_path},
-    #         )
-    # else:
-    #     form = ResumeUploadForm()
-    # return render(request, "candidates/upload_resume.html", {"form": form})
-
+  
 
 def candidate_form_view(request):
     print("Inside Candidate Form View")
-    # default_data = {
-    #     "name": "Amit Sanjay Jain",
-    #     "email": "amitsjain9161@gmail.com",
-    #     "phone": "+91 7020194397",
-    #     "current_city": "Pune",
-    #     "current_state": "Maharashtra",
-    #     "willing_to_relocate": True,
-    #     "citizenship": "Indian",
-    #     "professional_summery": "Results-driven Data Engineer...",
-    #     "preferred_roles": "Data Engineer, Palantir Foundry Specialist",
-    #     "preferred_locations": "Pune, Bangalore",
-    #     "total_experience": 4.0,
-    #     "relevant_experience": 2.0,
-    #     "languages_know": "Python, SQL",
-    #     "current_company": "Infosys Ltd",
-    #     "current_role": "Senior Associate Consultant",
-    #     "skills": "ETL Pipelines, Data Modeling",
-    #     "education": "Bachelor of Engineering in IT"
-    # }
-
-    # json_string = json.dumps(default_data)
-    # return HttpResponse(json_string, content_type='application/json')
-
-
-    # form = CandidateForm(initial=default_data)
-
-    # # return render(request, "candidates/candidate_form.html", {"form": form})
-
-    # if request.method == "POST":
-    #     form = CandidateForm(request.POST)
-    #     if form.is_valid():
-    #         form.save()
-    #         return render(request, "candidates/success.html")  # redirect or show success page
-
-    # return render(request, "candidates/candidate_form.html", {"form": form})
-
     parsed_data = request.session.get("parsed_data", {})  # retrieve parsed data
     form = CandidateForm(initial=parsed_data)  # pre-fill with parsed resume data
 
